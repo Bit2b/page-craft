25b44024f2218cb428b2eae585c4a2614f36ba9c
Manish <manishsahu6961@gmail.com>
2025-03-28 17:33:13 +0530

    added components in toolbar

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,5 +1,85 @@
-export const Editor=()=>{
-    return <div>
-        Editor
+'use client'
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image';
+import StarterKit from '@tiptap/starter-kit'
+import Underline from '@tiptap/extension-underline'
+import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style'
+import { Color } from '@tiptap/extension-color'
+import Highlight from '@tiptap/extension-highlight'
+import { useEditorStore } from '@/store/useEditorStore'
+
+
+export const Editor = () => {
+  const { setEditor } = useEditorStore();
+
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onTransaction({ editor }) {
+      setEditor(editor)
+    },
+    onFocus({ editor }) {
+      setEditor(editor)
+    },
+    onBlur({ editor }) {
+      setEditor(editor)
+    },
+    onContentError({ editor }) {
+      setEditor(editor)
+    },
+    editorProps: {
+      attributes: {
+        style: "padding-left:56px; padding-right:56px;",
+        class: "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] py-10 pr-14  cursor-text",
+      }
+    },
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      Highlight.configure({
+        multicolor: true
+      }),
+      Color,
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ImageResize,
+      Underline,
+      FontFamily,
+      TextStyle,
+    ],
+    content: `Hello World`,
+  })
+
+  return <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg:white print:overflow-visible'>
+    <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
+      <EditorContent editor={editor} />
     </div>
+  </div>
 }
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
 
 interface documentIdPageProps {
     params: Promise<{ documentId: string }>;
@@ -7,8 +8,8 @@ interface documentIdPageProps {
 const documentIdPage = async ({ params }: documentIdPageProps) => {
     const { documentId } = await params;
     return (
-        <div className="flex flex-col min-h-screen items-center justify-center gap-4 ">
-            <div>DocumentId : {documentId}</div>
+        <div className="flex flex-col min-h-screen items-center justify-center w-full">
+            <Toolbar/>
             <Editor />
         </div>
     )
